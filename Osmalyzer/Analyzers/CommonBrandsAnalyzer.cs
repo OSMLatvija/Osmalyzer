032bcd893d23d482f2662e6c06803a8e2dabf4c3
i-ky <gl.ivanovsky@gmail.com>
2023-06-14 21:35:26 +0300

    Use portable separators in path

--- Osmalyzer/Analyzers/CommonBrandsAnalyzer.cs
+++ Osmalyzer/Analyzers/CommonBrandsAnalyzer.cs
@@ -44,7 +44,7 @@ namespace Osmalyzer
             string nsiTagsFileName = @"NSI tags.tsv"; // from https://nsi.guide/?t=brands
 
             if (!File.Exists(nsiTagsFileName))
-                nsiTagsFileName = @"..\..\..\..\data\" + nsiTagsFileName; // "exit" Osmalyzer\bin\Debug\net6.0\ folder and grab it from root data\
+                nsiTagsFileName = @"../../../../data/" + nsiTagsFileName; // "exit" Osmalyzer\bin\Debug\net6.0\ folder and grab it from root data\
             
             string[] nsiRawTags = File.ReadAllLines(nsiTagsFileName);
 

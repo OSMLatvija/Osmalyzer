130484d0fd2ff64e749cc441e605c5a2e1cd91b5
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2023-06-27 15:23:48 +0300

    Extract GTFS analysis data superclass for reuse

---
+++ Osmalyzer/Data/GTFSAnalysisData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Osmalyzer
+{
+    public abstract class GTFSAnalysisData : PreparableAnalysisData
+    {
+        protected abstract string DataURL { get; }
+        
+        protected abstract string ExtractionFolder { get; }
+
+        
+        public override void Retrieve()
+        {
+            // Check if we have a data file cached
+            bool cachedFileOk = File.Exists(DataFileName);
+            
+            DateTime? newestDataDate = WebsiteDownloadHelper.ReadHeaderDate(DataURL);
+
+            if (newestDataDate == null)
+            {
+                Console.WriteLine("Data file did not have date header!");
+                cachedFileOk = false;
+            }
+            
+            if (cachedFileOk)
+            {
+                // Check that we actually know the date it was cached
+
+                if (DataDate == null)
+                {
+                    Console.WriteLine("Missing data date metafile!");
+                    cachedFileOk = false;
+                }
+            }
+
+            if (cachedFileOk)
+            {
+                // Check that we have the latest date
+
+                if (DataDate < newestDataDate)
+                {
+                    Console.WriteLine("Cached data out of date!");
+                    cachedFileOk = false;
+                }
+            }
+            
+            if (!cachedFileOk)
+            {
+                // Download latest (if anything is wrong)
+             
+                Console.WriteLine("Downloading...");
+                
+                WebsiteDownloadHelper.Download(
+                    DataURL, 
+                    DataFileName
+                );
+
+                if (newestDataDate != null)
+                    StoreDataDate(newestDataDate.Value);
+                else
+                    ClearDataDate(); // we couldn't get it
+            }
+        }
+
+        public override void Prepare()
+        {
+            // RS data comes in a zip file, so unzip
+            
+            ZipHelper.ExtractZipFile(DataFileName, ExtractionFolder + "/");
+        }
+    }
+}
--- Osmalyzer/Data/RigasSatiksmeDirectAnalysisData.cs
+++ Osmalyzer/Data/RigasSatiksmeDirectAnalysisData.cs
@@ -1,7 +1,3 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 
 namespace Osmalyzer
@@ -10,7 +6,7 @@ namespace Osmalyzer
     /// The data found on their website, seemingly whenever there are changes
     /// </summary>
     [UsedImplicitly]
-    public class RigasSatiksmeDirectAnalysisData : PreparableAnalysisData
+    public class RigasSatiksmeDirectAnalysisData : GTFSAnalysisData
     {
         public override string Name => "Rigas Satiksme";
         
@@ -21,69 +17,8 @@ namespace Osmalyzer
         public override bool? DataDateHasDayGranularity => true; // only day given on data page (file itself is month only)
 
         
-        public string ExtractionFolder => "RS";
+        protected override string ExtractionFolder => "RS";
         
-
-        public override void Retrieve()
-        {
-            string url = "https://saraksti.rigassatiksme.lv/riga/gtfs.zip";
-
-            // Check if we have a data file cached
-            bool cachedFileOk = File.Exists(DataFileName);
-            
-            DateTime? newestDataDate = WebsiteDownloadHelper.ReadHeaderDate(url);
-
-            if (newestDataDate == null)
-            {
-                Console.WriteLine("Data file did not have date header!");
-                cachedFileOk = false;
-            }
-            
-            if (cachedFileOk)
-            {
-                // Check that we actually know the date it was cached
-
-                if (DataDate == null)
-                {
-                    Console.WriteLine("Missing data date metafile!");
-                    cachedFileOk = false;
-                }
-            }
-
-            if (cachedFileOk)
-            {
-                // Check that we have the latest date
-
-                if (DataDate < newestDataDate)
-                {
-                    Console.WriteLine("Cached data out of date!");
-                    cachedFileOk = false;
-                }
-            }
-            
-            if (!cachedFileOk)
-            {
-                // Download latest (if anything is wrong)
-             
-                Console.WriteLine("Downloading...");
-                
-                WebsiteDownloadHelper.Download(
-                    url, 
-                    DataFileName
-                );
-
-                if (newestDataDate != null)
-                    StoreDataDate(newestDataDate.Value);
-                else
-                    ClearDataDate(); // we couldn't get it
-            }
-        }
-
-        public override void Prepare()
-        {
-            // RS data comes in a zip file, so unzip
-            
-            ZipHelper.ExtractZipFile(DataFileName, ExtractionFolder + "/");
-        }
+        protected override string DataURL => @"https://saraksti.rigassatiksme.lv/riga/gtfs.zip";
     }
 }

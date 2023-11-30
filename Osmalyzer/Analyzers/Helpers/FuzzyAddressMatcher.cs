6392f4c1a7e99f7a009a6b3d245890fcc03057f2
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2023-11-30 12:07:55 +0200

    Write some basic tests for fuzzy address matcher; fix two match issues

--- Osmalyzer/Analyzers/Helpers/FuzzyAddressMatcher.cs
+++ Osmalyzer/Analyzers/Helpers/FuzzyAddressMatcher.cs
@@ -1,84 +1,123 @@
 ﻿using System.Linq;
 using System.Text.RegularExpressions;
+using JetBrains.Annotations;
 
 namespace Osmalyzer;
 
 public static class FuzzyAddressMatcher
 {
-    public static bool Matches(OsmElement element, string address)
+    private static readonly string[] _suffixes = 
     {
-        address = address.Trim().ToLower();
+        "iela",
+        "bulvāris",
+        "ceļš",
+        "gatve",
+        "šoseja",
+        "tilts",
+        "dambis",
+        "aleja",
+        "apvedceļš",
+        "laukums",
+        "prospekts",
+        "pārvads"
+    };
+    
+    
+    [Pure]
+    public static bool Matches(OsmElement element, string fuzzyAddress)
+    {
+        return Matches(
+            element.GetValue("addr:street"),
+            element.GetValue("addr:housenumber"),
+            fuzzyAddress
+        );
+    }
+    
+    [Pure]
+    public static bool Matches(string? tagStreet, string? tagHouseNumber, string fuzzyAddress)
+    {
+        fuzzyAddress = fuzzyAddress.Trim().ToLower();
         
-        if (address == "")
+        if (fuzzyAddress == "")
             return false;
         
         // Street
         
-        string? street = element.GetValue("addr:street");
-        
-        if (street == null)
+        if (tagStreet == null)
             return false;
 
-        street = street.ToLower();
+        tagStreet = tagStreet.ToLower();
 
-        // Addresses often do "Sarkandaugavas 9" instead of a proper "Sarkandaugavas iela 9"
+        // Addresses often do "Ozolu 9" instead of a proper "Ozolu iela 9"
         
-        if (EndsWithStreetNameSuffix(street, out string? suffix))
-            street = street.Replace(suffix!, "").Trim();
+        if (EndsWithStreetNameSuffix(tagStreet, out string? tagSuffix))
+            tagStreet = tagStreet.Replace(tagSuffix!, "").Trim();
         
-        if (!address.Contains(street))
-            return false;
-        
+        if (!ContainsStreetNameSuffix(fuzzyAddress, out string? fuzzySuffix))
+        {
+            // We are something like "Ozolu 9"
+            if (!fuzzyAddress.Contains(tagStreet))
+                return false;
+        }
+        else
+        {
+            // We are something like "Ozolu gatve 9" but the tag is something like "Ozolu iela"
+            if (fuzzySuffix != tagSuffix)
+                return false;
+        }
+
         // TODO: "Kr.Barona"
         
         // Number
         
-        string? number = element.GetValue("addr:housenumber");
-
-        if (number == null)
+        if (tagHouseNumber == null)
             return false;
+        
+        tagHouseNumber = tagHouseNumber.ToLower();
 
         // Don't know where the number is, but let's hope if somewhere 
         
-        MatchCollection matches = Regex.Matches(address, @"\d+[a-z]?");
+        MatchCollection matches = Regex.Matches(fuzzyAddress, @"\d+[a-z]?", RegexOptions.IgnoreCase);
         // 13
         // 13B
 
         if (matches.Count == 0)
             return false;
 
-        if (matches.All(m => m.ToString().ToLower() != number))
+        if (matches.All(m => m.ToString().ToLower() != tagHouseNumber))
             return false;
 
         return true;
     }
 
+    [Pure]
     public static bool EndsWithStreetNameSuffix(string streetName)
     {
         return EndsWithStreetNameSuffix(streetName, out string _);
     }
     
+    [Pure]
     public static bool EndsWithStreetNameSuffix(string streetName, out string? suffix)
     {
-        string[] suffixes = 
-        {
-            "iela",
-            "bulvāris",
-            "ceļš",
-            "gatve",
-            "šoseja",
-            "tilts",
-            "dambis",
-            "aleja",
-            "apvedceļš",
-            "laukums",
-            "prospekts",
-            "pārvads"
-        };
-
-        foreach (string s in suffixes)
+        foreach (string s in _suffixes)
         {
             if (streetName.EndsWith(s))
+            {
+                suffix = s;
+                return true;
+            }
+        }
+
+        suffix = null;
+        return false;
+    }
+
+    [Pure]
+    public static bool ContainsStreetNameSuffix(string address, out string? suffix)
+    {
+        foreach (string s in _suffixes)
+        {
+            if (address.Contains(s))
             {
                 suffix = s;
                 return true;
---
+++ OsmalyzerTests/FuzzyAddressMatcherTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using Osmalyzer;
+
+namespace OsmalyzerTests;
+
+public class FuzzyAddressMatcherTests
+{
+    [TestCase("Rīgas gatve 15", "Rīgas gatve", "15")]
+    [TestCase("Rīgas Gatve 15", "Rīgas gatve", "15")]
+    [TestCase("15, Rīgas gatve", "Rīgas gatve", "15")]
+    [TestCase("TC Buyall, Rīgas gatve 15, Rīga", "Rīgas gatve", "15")]
+    [TestCase("Rīgas gatve 15D", "Rīgas gatve", "15D")]
+    [TestCase("Rīgas gatve 15d", "Rīgas gatve", "15D")]
+    [TestCase("Brīvības 15", "Brīvības iela", "15")]
+    public void TestMatches(string fullAddress, string tagStreet, string tagHouseNumber)
+    {
+        bool doesMatch = FuzzyAddressMatcher.Matches(tagStreet, tagHouseNumber, fullAddress);
+
+        Assert.IsTrue(doesMatch);
+    }    
+    
+    [TestCase("Rīgas gatve 37", "Rīgas gatve", "15")]
+    [TestCase("Rīgas iela 15", "Rīgas gatve", "15")]
+    public void TestDoesntMatch(string fullAddress, string tagStreet, string tagHouseNumber)
+    {
+        bool doesMatch = FuzzyAddressMatcher.Matches(tagStreet, tagHouseNumber, fullAddress);
+
+        Assert.IsFalse(doesMatch);
+    }
+}

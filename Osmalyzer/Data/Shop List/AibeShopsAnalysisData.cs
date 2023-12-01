a63011c484daf190de3f5f4ae2a7316530cd2057
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2023-12-01 14:06:56 +0200

    Parse Aibe POI type from data and add company name; besides semi-random franchised/partner shops, there are also bars and gas stations - exclude for now

--- Osmalyzer/Data/Shop List/AibeShopsAnalysisData.cs
+++ Osmalyzer/Data/Shop List/AibeShopsAnalysisData.cs
@@ -33,13 +33,14 @@ public class AibeShopsAnalysisData : ShopListAnalysisData
         );
     }
 
+    
     public override void Prepare()
     {
         string source = File.ReadAllText(DataFileName);
         
         MatchCollection matches = Regex.Matches(
             source, 
-            @"\[([\d\.]+),([\d\.]+),""([^""]+)"",[^\]]+\]," // todo: can capture more, but it's all cryptic data
+            @"\[([\d\.]+),([\d\.]+),""([^""]+)"",""([^""]+)"",[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,""([^""]+)"",[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+\]"
         );
         
         // [56.95491,24.19849,"Dzelzavas iela 76, R\u012bga","V-980, Ankami SIA","08:00","22:00","08:00","22:00","08:00","22:00","R\u012bga","riga","partner","false","false","0","Partneris","29619824","","riga","","","","","","","riga"]
@@ -48,20 +49,62 @@ public class AibeShopsAnalysisData : ShopListAnalysisData
             throw new Exception("Did not match any items on webpage");
         
         _shops = new List<ShopData>();
-                
+
         foreach (Match match in matches)
         {
             double lat = double.Parse(match.Groups[1].ToString());
+            
             double lon = double.Parse(match.Groups[2].ToString());
-            string address = Regex.Unescape(match.Groups[3].ToString());
+            
+            string address = Regex.Unescape(match.Groups[3].ToString().Trim());
+            
+            string? company = Regex.Unescape(match.Groups[4].ToString().Trim());
+            if (company == "")
+            {
+                company = null;
+            }
+            else
+            {
+                company = Regex.Replace(company, @"V-\d+,\s*", ""); // prefix for something unknown "V-960, Falko-2 SIA"
+                if (company.ToLower().Contains("aibe")) company = null; // i.e. self - "Aibe pārtika SIA";
+            }
 
-            _shops.Add(
-                new ShopData(
-                    "Aibe", 
-                    address, 
-                    new OsmCoord(lat, lon)
-                )
-            );
+            ShopType shopType = ShopTypeFromRaw(match.Groups[5].ToString().Trim());
+
+            if (shopType is ShopType.Shop or ShopType.Partner) // todo: gas and bar? these have different tagging in osm
+            {
+                _shops.Add(
+                    new ShopData(
+                        "Aibe",
+                        (company != null ? company + ", " : "") + address,
+                         new OsmCoord(lat, lon)
+                    )
+                );
+            }
         }
+    }
+
+    
+    private ShopType ShopTypeFromRaw(string rawType)
+    {
+        return rawType switch
+        {
+            "shop"    => ShopType.Shop,
+            "Veikals" => ShopType.Shop, // looks like a manual input error
+            "partner" => ShopType.Partner,
+            "bar"     => ShopType.Bar,
+            "gas"     => ShopType.Gas,
+
+            _ => throw new ArgumentOutOfRangeException(nameof(rawType), rawType, null)
+        };
+    }
+
+    
+    private enum ShopType
+    {
+        Shop,
+        Partner,
+        Bar,
+        Gas
     }
 }

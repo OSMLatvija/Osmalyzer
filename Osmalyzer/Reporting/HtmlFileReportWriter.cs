89bce030dccdea825dad48ccfcd1ba2a03787ed4
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2023-12-03 12:51:22 +0200

    Remove "location" label from OSM location links - it's patently obvious from it being a coord

--- Osmalyzer/Reporting/HtmlFileReportWriter.cs
+++ Osmalyzer/Reporting/HtmlFileReportWriter.cs
@@ -256,7 +256,7 @@ public class HtmlFileReportWriter : ReportWriter
         line = Regex.Replace(line, @"(https://osm.org/way/(\d+))", @"<a href=""$1"" class=""osm-link"" target=\""_blank\"">Way $2</a>");
         line = Regex.Replace(line, @"(https://osm.org/relation/(\d+))", @"<a href=""$1"" class=""osm-link"" target=\""_blank\"">Rel $2</a>");
         line = Regex.Replace(line, @"(https://osm.org/changeset/(\d+))", @"<a href=""$1"" class=""osm-link"" target=\""_blank\"">Changeset $2</a>");
-        line = Regex.Replace(line, @"(https://osm.org/#map=\d{1,2}/(-?\d{1,3}\.\d+)/(-?\d{1,3}\.\d+))", @"<a href=""$1"" class=""osm-link"" target=\""_blank\"">Location $2, $3</a>");
+        line = Regex.Replace(line, @"(https://osm.org/#map=\d{1,2}/(-?\d{1,3}\.\d+)/(-?\d{1,3}\.\d+))", @"<a href=""$1"" class=""osm-link"" target=\""_blank\"">$2, $3</a>");
             
         line = Regex.Replace(line, @"(https://overpass-turbo.eu/\?Q=[a-zA-Z0-9%\-_\.!*()+]+)", @"<a href=""$1"" target=\""_blank\"">Query</a>");
 

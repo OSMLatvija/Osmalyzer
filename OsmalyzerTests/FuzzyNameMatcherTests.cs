1477336deb781779d6903de29aabfc6678cb235b
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2023-12-05 00:58:44 +0200

    Add empty unit tests for fuzzy name matcher planning to design some better matching

---
+++ OsmalyzerTests/FuzzyNameMatcherTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Osmalyzer;
+
+namespace OsmalyzerTests;
+
+public class FuzzyNameMatcherTests
+{
+    [TestCase("A", "A")]
+    [TestCase("A", "a")]
+    [TestCase("a", "a")]
+    [TestCase("a", "A")]
+    public void TestMatches(string name1, string name2)
+    {
+        bool doesMatch = FuzzyNameMatcher.Matches(name1, name2);
+
+        Assert.IsTrue(doesMatch);
+    }    
+    
+    [TestCase("A", "B")]
+    public void TestDoesntMatch(string name1, string name2)
+    {
+        bool doesMatch = FuzzyNameMatcher.Matches(name1, name2);
+
+        Assert.IsFalse(doesMatch);
+    }
+}

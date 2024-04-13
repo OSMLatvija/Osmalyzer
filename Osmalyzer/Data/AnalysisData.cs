92a4ed94793f53bd8a3bd8f0e124e95a4426bc51
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2024-04-13 15:08:14 +0300

    Looks like a data problem that I have no way of circumventing

--- Osmalyzer/Data/AnalysisData.cs
+++ Osmalyzer/Data/AnalysisData.cs
@@ -71,16 +71,6 @@ public abstract class AnalysisData
 
     public void Prepare()
     {
-        // TODO: TEMP
-        // TODO: TEMP
-        // TODO: TEMP
-        
-        DoPrepare();
-        
-        Status = DataStatus.Ok;
-
-        return;
-        
         try
         {
             DoPrepare();

0f80493425db28638190ce1ea6a6d2cf7f9f535e
Aliaksei Markouski <alexey.markouski@gmail.com>
2024-05-28 16:19:50 +0300

    Add a bit of explanation to report description

--- Osmalyzer/Analyzers/Parcel Lockers/ParcelLockerAnalyzer.cs
+++ Osmalyzer/Analyzers/Parcel Lockers/ParcelLockerAnalyzer.cs
@@ -10,7 +10,7 @@ public abstract class ParcelLockerAnalyzer<T> : Analyzer where T : ParcelLockerA
     public override string Name => Operator + " Parcel lockers";
 
     public override string Description => "This report checks that all " + Operator + " parcel lockers listed on company's website are found on the map." + Environment.NewLine +
-                                          "Note that parcel locker websites can and do have errors.";
+                                          "Note that parcel locker websites can and do have errors: mainly incorrect position, but sometimes lockers are missing too.";
 
     public override AnalyzerGroup Group => AnalyzerGroups.ParcelLocker;
 

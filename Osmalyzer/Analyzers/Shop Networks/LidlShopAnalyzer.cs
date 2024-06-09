ef0c953dd21a5ce77e4223e71878fd26361cb97e
Aliaksei Markouski <alexey.markouski@gmail.com>
2024-06-09 21:36:08 +0300

    Add Lidl Analyzer

---
+++ Osmalyzer/Analyzers/Shop Networks/LidlShopAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Osmalyzer;
+
+public class LidlShopAnalyzer : ShopAnalyzer<LidlShopsAnalysisData>
+{
+    protected override string ShopName => "Lidl";
+
+    protected override List<string> ShopOsmNames => new List<string>() { ShopName };
+}
--- Osmalyzer/Runner.cs
+++ Osmalyzer/Runner.cs
@@ -72,6 +72,7 @@ public static class Runner
             // new LatviaPostLockerAnalyzer(),
             // new LatviaPostMailBoxAnalyzer(),
             // new ImproperTranslationAnalyzer(),
+            // new LidlShopAnalyzer(),
             new SpellingAnalyzer()
         };
 #endif

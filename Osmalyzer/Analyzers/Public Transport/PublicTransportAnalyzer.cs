12552b4036610deee86d2bfbfcd7b617cf82b073
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2025-05-04 20:36:55 +0300

    Optimize stop name comparing with caching generic cleaned names

--- Osmalyzer/Analyzers/Public Transport/PublicTransportAnalyzer.cs
+++ Osmalyzer/Analyzers/Public Transport/PublicTransportAnalyzer.cs
@@ -1,8 +1,6 @@
-﻿using System.Diagnostics;
+﻿namespace Osmalyzer;
 
-namespace Osmalyzer;
-
-public abstract class PublicTransportAnalyzer<T> : Analyzer
+public abstract class PublicTransportAnalyzer<T> : PublicTransportAnalyzerBase
     where T : GTFSAnalysisData, new()
 {
     public override string Description => "This checks the public transport route issues for " + Name;
@@ -536,112 +534,8 @@ public abstract class PublicTransportAnalyzer<T> : Analyzer
     private record RoutePair(OsmRelation OsmRoute, RouteVariant RouteVariant, List<StopPair> Stops, float Score);
     
     private record StopPair(GTFSStop RouteStop, OsmElement OsmStop);
-
-    // private (GTFSTrip service, float bestMatch) FindBestTripMatch(GTFSRoute route, List<OsmNode> stops)
-    // {
-    //     // I have to do fuzyz matching, because I don't actually know which service and which trip OSM is representing
-    //     // That is, I don't know which GTFS trip is the regular normal trip and which are random depo and alternate trips
-    //     // OSM may even have alternate trips, so I need to match them in that case
-    //     // So this matches the route with the best stop match - more stop matches, better match
-    //                     
-    //     GTFSTrip? bestTrip = null;
-    //     float bestMatch = 0f;
-    //                     
-    //     foreach (GTFSService ptService in route.Services)
-    //     {
-    //         foreach (GTFSTrip ptTrip in ptService.Trips)
-    //         {
-    //             int matchedStops = 0;
-    //
-    //             foreach (GTFSStop ptStop in ptTrip.Stops)
-    //             {
-    //                 if (fullyMatchedStops.TryGetValue(ptStop, out OsmNode? expectedOsmStop))
-    //                 {
-    //                     if (stops.Contains(expectedOsmStop))
-    //                         matchedStops++;
-    //                 }
-    //             }
-    //
-    //             float match = Math.Max(0f, (float)matchedStops / Math.Max(ptTrip.Points.Count(), stops.Count));
-    //
-    //             if (bestTrip == null ||
-    //                 match > bestMatch)
-    //             {
-    //                 bestMatch = match;
-    //                 bestTrip = ptTrip;
-    //             }
-    //         }
-    //     }
-    //
-    //     return (bestTrip!, bestMatch); // will always match at least something, even if 0%
-    // }
-    //
-    //
-    // [Pure]
-    // private static bool MatchesRoute(OsmRelation osmRoute, GTFSRoute route)
-    // {
-    //     // OSM
-    //     // from	Preču 2
-    //     // name	Bus 13: Preču 2 => Kleisti => Babītes stacija
-    //     // public_transport:version	2
-    //     // ref	13
-    //     // roundtrip	no
-    //     // route	bus
-    //     // to	Babītes stacija
-    //     // type	route
-    //     // via	Kleisti
-    //         
-    //     // RS
-    //     // riga_bus_13,"13","Babītes stacija - Kleisti - Preču 2",,3,https://saraksti.rigassatiksme.lv/index.html#riga/bus/13,F4B427,FFFFFF,2001300
-    //
-    //     if (route.Type != osmRoute.GetValue("route"))
-    //         return false;
-    //         
-    //     if (route.Number != osmRoute.GetValue("ref"))
-    //         return false;
-    //
-    //     string? osmName = osmRoute.GetValue("name");
-    //
-    //     if (osmName == null)
-    //         return false;
-    //
-    //     string[] split = route.Name.Split('-');
-    //
-    //     int count = 0;
-    //         
-    //     foreach (string s in split)
-    //     {
-    //         string ptName = s.Trim();
-    //
-    //         if (OsmNameHasPTNamePart(osmName, ptName))
-    //             count++;
-    //
-    //         // TODO: This will probably need matching GTFS<->OSM name matching
-    //     }
-    //
-    //     if (count < 2)
-    //         return false;
-    //         
-    //     // Some are Abrenes iela - Jaunciems - Suži
-    //     // Some are Abrenes iela - Jaunmārupe
-    //
-    //     return true; // Guess it's "good enough" without more complex checks
-    //         
-    //         
-    //     [Pure]
-    //     static bool OsmNameHasPTNamePart(string osmName, string ptName)
-    //     {
-    //         // Straight match
-    //         if (osmName.Contains(ptName))
-    //             return true;
-    //             
-    //         // TEC 2 vs TEC-2
-    //         if (osmName.Replace('-', ' ').Contains(ptName))
-    //             return true;
-    //
-    //         return false;
-    //     }
-    // }
+    
+    
     //
     // [Pure]
     // private static StopNameMatching DoStopsMatch(OsmNode osmStop, GTFSStop ptStop)
@@ -689,39 +583,75 @@ public abstract class PublicTransportAnalyzer<T> : Analyzer
     [Pure]
     private static bool IsStopNameMatchGoodEnough(string ptStopName, string osmStopName)
     {
-        // Stops never differ by capitalization, so just lower them and avoid weird capitalization in addition to everything else
-        // Rezekne "18.Novembra iela" vs OSM "18. novembra iela"
-        ptStopName = ptStopName.ToLower();
-        osmStopName = osmStopName.ToLower();
-            
         // Quick check first, may be we don't need to do anything
         if (ptStopName == osmStopName)
             return true;
+
+        if (CleanedStopNameCache.TryGetValue(ptStopName, out string? cleanedPtStopName))
+        {
+            // We already cleaned this name, so use it
+            ptStopName = cleanedPtStopName;
+        }
+        else
+        {
+            // Clean the name and store it for later
+            cleanedPtStopName = CleanName(ptStopName);
+            CleanedStopNameCache[ptStopName] = cleanedPtStopName;
+            ptStopName = cleanedPtStopName;
+        }
+
+        if (CleanedStopNameCache.TryGetValue(osmStopName, out string? cleanedOsmStopName))
+        {
+            // We already cleaned this name, so use it
+            osmStopName = cleanedOsmStopName;
+        }
+        else
+        {
+            // Clean the name and store it for later
+            cleanedOsmStopName = CleanName(osmStopName);
+            CleanedStopNameCache[osmStopName] = cleanedOsmStopName;
+            osmStopName = cleanedOsmStopName;
+        }
+        
+
+        [Pure]
+        static string CleanName(string name)
+        {
+            // Stops never really differ by capitalization, so just lower them and avoid weird capitalization in addition to everything else
+            // Rezekne "18.Novembra iela" vs OSM "18. novembra iela"
+            name = name.ToLower();
             
-        // Trim parenthesis from OSM
-        // Jurmalas OSM stops have a lot of parenthesis, like JS "Majoru stacija" vs OSM "Majoru stacija (Majori)"
-        osmStopName = Regex.Replace(osmStopName, @" \([^\(\)]+\)$", @"");
-        // todo: return if the match was poor quality this way and the name should be checked
-        // todo: what if GTFS data DOES have the parenthesis?
-    
-        // Trim brackets from GTFS
-        // A couple Jurmalas GTFS stops have brackets, like JS "Promenādes iela [Promenādes iela]" and "Promenādes iela [Rīgas iela]" vs OSM "Promenādes iela" and "Promenādes iela"
-        ptStopName = Regex.Replace(ptStopName, @" \[[^\[\]]+\]$", @"");
+            // Trim parenthesis from OSM
+            // Jurmalas OSM stops have a lot of parenthesis, like JS "Majoru stacija" vs OSM "Majoru stacija (Majori)"
+            name = Regex.Replace(name, @" \([^\(\)]+\)$", @"");
             
-        // Both OSM and RS stops are inconsistent about spacing around characters
-        // "2.trolejbusu parks" or "Jaunciema 2.šķērslīnija" (also all the abbreviated "P.Lejiņa iela" although this won't match)
-        // "Upesgrīvas iela/ Spice"
-        // OSM "TEC-2 pārvalde" vs RS "TEC- 2 pārvalde" or OSM "Preču-2" vs RS "Preču - 2"
-        if (Regex.Replace(Regex.Replace(osmStopName, @"([\./-])(?! )", @"$1 "), @"(?<! )([\./-])", @" $1") == 
-            Regex.Replace( Regex.Replace(ptStopName, @"([\./-])(?! )", @"$1 "), @"(?<! )([\./-])", @" $1"))
+            // Trim brackets from GTFS
+            // A couple Jurmalas GTFS stops have brackets, like JS "Promenādes iela [Promenādes iela]" and "Promenādes iela [Rīgas iela]" vs OSM "Promenādes iela" and "Promenādes iela"
+            name = Regex.Replace(name, @" \[[^\[\]]+\]$", @"");
+
+            // Sometimes proper quotes are inconsistent between the two
+            // OSM "Arēna "Rīga"" vs RS "Arēna Rīga" or OSM ""Bērnu pasaule"" vs RS "Bērnu pasaule"
+            // or opposite OSM "Dzintars" vs RS ""Dzintars""
+            name = name.Replace("\"", "");
+            
+            // Both OSM and RS stops are inconsistent about spacing around characters
+            // "2.trolejbusu parks" or "Jaunciema 2.šķērslīnija" also all the abbreviated "P.Lejiņa iela"
+            // "Upesgrīvas iela/ Spice"
+            // OSM "TEC-2 pārvalde" vs RS "TEC- 2 pārvalde" or OSM "Preču-2" vs RS "Preču - 2"
+            name = Regex.Replace(name, @"(?<! )([\./-])", @" $1");
+            name = Regex.Replace(name, @"([\./-])(?! )", @"$1 ");
+
+            return name;
+        }
+        
+
+        // Are cleaned names equal?
+        if (ptStopName == osmStopName)
             return true;
-            
-        // Sometimes proper quotes are inconsistent between the two
-        // OSM "Arēna "Rīga"" vs RS "Arēna Rīga" or OSM ""Bērnu pasaule"" vs RS "Bērnu pasaule"
-        // or opposite OSM "Dzintars" vs RS ""Dzintars""
-        if (osmStopName.Replace("\"", "") == ptStopName.Replace("\"", ""))
-            return true;
-            
+        
+        
+        // Special checks
+        
         // RS likes to abbreviate names for stops while OSM spells them out
         // OSM "Eduarda Smiļģa iela" vs RS "E.Smiļģa iela"
         // Because there are so many like this, I will consider them correct for now, even if they aren't technically accurate 
@@ -757,13 +687,6 @@ public abstract class PublicTransportAnalyzer<T> : Analyzer
         return false;
     }
     
-    //
-    // private enum StopNameMatching // order used by sorting
-    // {
-    //     Mismatch = 0,
-    //     WeakMatch = 1,
-    //     Match = 2
-    // }
     
     private enum GroupDesignation
     {
---
+++ Osmalyzer/Analyzers/Public Transport/PublicTransportAnalyzerBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Osmalyzer;
+
+public abstract class PublicTransportAnalyzerBase : Analyzer
+{
+    protected static Dictionary<string, string> CleanedStopNameCache { get; } = [ ];
+    // In base class because generic inheritors won't share this, i.e. PTA<RS>.cache != PTA<JAP>.cache, but we want to share cache
+}

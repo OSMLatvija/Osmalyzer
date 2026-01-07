50acac4dfc11a11773d7696bc8a768638a866337
HellMapGoesCoding <111684839+HellMapGoesCoding@users.noreply.github.com>
2026-01-07 16:51:03 +0200

    JOSM shows id as 0 even though output isn't, but double check it's not for future

--- Core/Change/OsmChangeAction.cs
+++ Core/Change/OsmChangeAction.cs
@@ -1,8 +1,17 @@
-﻿namespace Osmalyzer;
+﻿using System;
+
+namespace Osmalyzer;
 
 public abstract record OsmChangeAction(OsmElement Element);
 
-public record OsmChangeCreateAction(OsmElement Element) : OsmChangeAction(Element);
+public record OsmChangeCreateAction : OsmChangeAction
+{
+    public OsmChangeCreateAction(OsmElement Element)
+        : base(Element)
+    {
+        if (Element.Id >= 0) throw new ArgumentException("Element to create must have a temporary (negative) ID.");
+    }
+}
 
 public record OsmChangeModifyAction(OsmElement Element) : OsmChangeAction(Element);
 
